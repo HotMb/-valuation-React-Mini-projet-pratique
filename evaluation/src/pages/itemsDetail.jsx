a0a59c5da4cd880ee0e4c6461ace22a4fcd01e09
Otmane Mabrouk <otmanmabrouk01@gmail.com>
2025-12-17 16:30:00 +0100

    itemsDetail modifier avec un button pour revenir a la page items

--- evaluation/src/pages/itemsDetail.jsx
+++ evaluation/src/pages/itemsDetail.jsx
@@ -1,8 +1,13 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { SquareArrowLeft } from 'lucide-react';
+
 function ItemsDetail() {
     const { id } = useParams();
     console.log(id);
-    return <div>Détail de l'élément {id}</div>;
+    return <div>
+        <Link to={`/items`}><SquareArrowLeft />Revenir a la page precedente</Link>
+        <p>Détail de l'élément {id}</p>
+        </div>;
 }
 
 export default ItemsDetail;
